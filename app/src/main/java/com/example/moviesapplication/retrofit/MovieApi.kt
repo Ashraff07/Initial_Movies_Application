fce1c746738455a079abf5343a435c9f7780e343
Ashraff Shaik <ashraff.x.shaik@kp.org>
2024-03-29 06:49:33 +0530

    Git blame

--- app/src/main/java/com/example/moviesapplication/retrofit/MovieApi.kt
+++ app/src/main/java/com/example/moviesapplication/retrofit/MovieApi.kt
@@ -1,13 +1,13 @@
 package com.example.moviesapplication.retrofit
 
 import com.example.moviesapplication.model.Movies
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
-const val apikey:String = "69d66957eebff9666ea46bd464773cf0"
+const val apikey : String = "69d66957eebff9666ea46bd464773cf0"
 
 interface MovieApi {
+    // added this comment to remove password
     @GET("popular?")
     suspend fun getPopularMovies(@Query("api_key") api_key : String) : Response<Movies>
 }
